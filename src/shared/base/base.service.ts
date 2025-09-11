556bbea8e1f1933ef9aa49a0d8555e75e139443c
sasha125588 <sashavolskiy11@gmail.com>
2025-09-11 23:23:32 +0300

    add base classes for CRUD

---
+++ src/shared/base/base.service.ts
@@ -0,0 +1,43 @@
+import type { Repository } from 'typeorm';
+
+export class BaseService<T> {
+  constructor(private repository: Repository<T>) {}
+
+  find(...params: Parameters<typeof this.repository.find>) {
+    return this.repository.find(...params);
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  async findOne(...params: Parameters<typeof this.repository.findOne>) {
+    return await this.repository.findOne(...params);
+  }
+
+  save(...params: Parameters<typeof this.repository.save>) {
+    return this.repository.save(...params);
+  }
+
+  insert(...params: Parameters<typeof this.repository.insert>) {
+    return this.repository.insert(...params);
+  }
+
+  update(...params: Parameters<typeof this.repository.update>) {
+    return this.repository.update(...params);
+  }
+
+  delete(...params: Parameters<typeof this.repository.delete>) {
+    return this.repository.delete(...params);
+  }
+
+  clear(...params: Parameters<typeof this.repository.clear>) {
+    return this.repository.clear(...params);
+  }
+
+  createQueryBuilder(
+    ...params: Parameters<typeof this.repository.createQueryBuilder>
+  ) {
+    return this.repository.createQueryBuilder(...params);
+  }
+}
